0b99bf2b749ae0a0bf9c9900696386c0b4e916bd
Vraj Patel <vrajp1052005@gmail.com>
2026-04-12 01:50:35 -0400

    load_data function

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ configurations = {
     'src_max_seq_len' : 323,
     'tgt_max_seq_len' : 123,
     'src_vocab_size' : 10000,
-    'tgt_vocab_size' : 8000
-    
+    'tgt_vocab_size' : 8000,
+    'path' : 'C:\My Projects\Transformer from scratch without any help - Vraj\data\Sentence pairs in English-Hindi - 2026-04-12.tsv'
 }
--- train.py
+++ train.py
@@ -1,3 +1,15 @@
 import torch
 import torch.nn as nn
+import pandas as pd
 
+from config import configurations
+
+
+
+path = configurations['path']
+
+custom_cols = ["col1", "english_sentence", "col2", "hindi_sentence"]
+def load_data(path):
+  data = pd.read_csv(path, sep='\t', header=None, names=custom_cols, nrows=640, encoding='utf-8')
+  data.drop(columns=['col1', 'col2'], inplace=True)
+  return data['english_sentence'].tolist(), data['hindi_sentence'].tolist()
