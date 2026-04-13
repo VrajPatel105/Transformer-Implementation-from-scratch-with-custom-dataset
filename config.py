2a6b3e8cbf6b54f8102067cfbc339816312cfad9
Vraj Patel <vrajp1052005@gmail.com>
2026-04-12 20:08:20 -0400

    moving the custom_col list into load_data func

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ configurations = {
     'tgt_max_seq_len' : 123,
     'src_vocab_size' : 10000,
     'tgt_vocab_size' : 8000,
-    'path' : 'C:\My Projects\Transformer from scratch without any help - Vraj\data\Sentence pairs in English-Hindi - 2026-04-12.tsv'
+    'path' : 'C:\My Projects\Transformer from scratch without any help - Vraj\data\English-German.tsv'
 }
--- train.py
+++ train.py
@@ -1,16 +1,29 @@
 import torch
 import torch.nn as nn
 import pandas as pd
+from torch.utils.data import Dataset, DataLoader
 
 from config import configurations
 
 
-
 path = configurations['path']
 
-custom_cols = ["col1", "english_sentence", "col2", "hindi_sentence"]
 def load_data(path):
-  data = pd.read_csv(path, sep='\t', header=None, names=custom_cols, nrows=640, encoding='utf-8')
+  custom_cols = ["col1", "english_sentence", "col2", "german_sentence"]
+
+  data = pd.read_csv(path, sep='\t', header=None, names=custom_cols, encoding='utf-8')
   data.drop(columns=['col1', 'col2'], inplace=True)
-  return data['english_sentence'].tolist(), data['hindi_sentence'].tolist()
+  return data['english_sentence'].tolist(), data['german_sentence'].tolist()
+
+# Subclass torch.utils.data.Dataset
+# __init__ stores everything you'll need (sentences, tokenizers, max_len)
+# __len__ returns how many pairs
+# __getitem__(idx):
+
+# Encode English with SOS + EOS
+# Encode German with SOS only → this is decoder_input
+# Encode German with EOS only → this is label
+# Pad all three to max_len with PAD_ID
+# Convert to torch.tensor(..., dtype=torch.long)
+# Return as a dict
 
